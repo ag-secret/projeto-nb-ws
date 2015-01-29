9b85c5a3a8408fc1e33c87ad2561cbf1e8873b0e
Daniel Pedro <ag.interagir@gmail.com>
2015-01-29 10:08:18 -0200

    teste de cors

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
-
-require 'webroot' .DIRECTORY_SEPARATOR. 'index.php';
-//te
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    header("Access-Control-Allow-Headers', 'Authorization, X-Authorization, Origin, Accept, Content-Type, X-Requested-With, X-HTTP-Method-Override");
+}
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])){
+        header("Access-Control-Allow-Methods: GET, PUT, DELETE, OPTIONS");
+    }
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])){
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    }
+    exit(0);
+}
+//require 'webroot' .DIRECTORY_SEPARATOR. 'index.php';
